4b4fe96c12147776d2e547ea909de9775c81aad7
masumahemd <rimasum01724@gmail.com>
2023-09-11 22:23:09 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     
+      <h1 className='text-5xl bg-red-300 p-4 font-semibold' >Knowledge Cafe</h1>
+     
+     
+    </>
+  )
+}
+
+export default App
